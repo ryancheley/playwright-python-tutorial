356333b5f3c7358bb29e41ff6e1908682b6cac3a
Ryan Cheley <rcheley@gmail.com>
2022-11-19 15:50:04 -0800

    :tada: Initial Commit Based on the tutorial found here: https://github.com/AutomationPanda/playwright-python-tutorial/blob/main/tutorial/1-getting-started.md

---
+++ tests/test_search.py
@@ -0,0 +1,7 @@
+def test_basic_duckduckgo_search() -> None:
+    # Given the DuckDuckGo home page is displayed
+    # When the user searches for a phrase
+    # Then the search result query is the phrase
+    # And the search result links pertain to the phrase
+    # And the search result title contains the phrase
+    pass
