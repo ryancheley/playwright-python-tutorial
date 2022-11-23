59c661aeec7f13bc0f0df2bd03b86e40b9a92612
Ryan Cheley <rcheley@gmail.com>
2022-11-22 20:15:31 -0800

    Completed First Steps

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,17 @@
-def test_basic_duckduckgo_search() -> None:
+from playwright.sync_api import Page
+
+def test_basic_duckduckgo_search(page: Page) -> None:
     # Given the DuckDuckGo home page is displayed
+    page.goto('https://www.duckduckgo.com', wait_until='networkidle')
+
     # When the user searches for a phrase
+    page.locator('#search_form_input_homepage').fill('panda')
+    page.locator('#search_button_homepage').click()
+    
     # Then the search result query is the phrase
+
     # And the search result links pertain to the phrase
+
     # And the search result title contains the phrase
-    pass
+
+    
